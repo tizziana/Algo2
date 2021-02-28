47b353e6cf8d76901748c13aad166efd065c3fd3
Tizziana Mazza Reta <51216329+tizziana@users.noreply.github.com>
2021-02-28 17:30:45 -0300

    Add files via upload

---
+++ 00-tp0/tp0.c
@@ -0,0 +1,82 @@
+#include "tp0.h"
+
+/* ******************************************************************
+ *                     FUNCIONES A COMPLETAR
+ * *****************************************************************/
+
+/* swap() intercambia dos valores enteros.
+ */
+void swap (int* x, int* y) {
+
+	int puntero_aux = *x;
+	*x = *y;
+	*y = puntero_aux;
+}
+
+
+/* maximo() busca el mayor elemento del arreglo y devuelve su posicion.
+ * Si el vector es de largo 0, devuelve -1.
+ */
+int maximo(int vector[], int n) {
+
+	if (n == 0){
+		return -1;
+	}
+
+	int posicion_maximo = 0;
+
+	for (int i = 1; i < n; i++) {
+		if (vector[i] > vector[posicion_maximo]) {
+			posicion_maximo = i;
+		}
+	}
+
+	return posicion_maximo;
+}
+
+
+/* La función comparar recibe dos vectores y sus respectivas longitudes
+ * y devuelve -1 si el primer vector es menor que el segundo; 0 si son
+ * iguales; y 1 si el segundo es menor.
+ *
+ * Un vector es menor a otro cuando al compararlos elemento a elemento,
+ * el primer elemento en el que difieren no existe o es menor.
+ */
+int comparar(int vector1[], int n1, int vector2[], int n2) {
+
+	int i;
+
+	for (i = 0; i < n1 && i < n2; i++) {
+
+		if (vector1[i] < vector2[i]) {
+			return -1;
+		}
+
+		if(vector1[i] > vector2[i]) {
+			return 1;
+		}
+	}
+
+	if (n1 < n2){
+		return -1;
+	}
+	if (n1 > n2){
+		return 1;
+	}
+	
+	return 0;
+}
+
+
+/* selection_sort() ordena el arreglo recibido mediante el algoritmo de
+ * selección.
+ */
+void seleccion(int vector[], int n) {
+	
+	for (int j = n - 1; j > 0; j--){
+		
+		int pos_max = maximo(vector, j + 1);
+		swap(&vector[j], &vector[pos_max]);
+	}
+
+}
